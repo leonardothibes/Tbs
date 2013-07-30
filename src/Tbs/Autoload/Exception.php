06a838ed55519cf7676b3a28b7b87824a4b259af
Leonardo Thibes <eu@leonardothibes.com>
2013-07-30 11:28:21 -0300

    adicionando tratamento de excessao ao autoloader

---
+++ src/Tbs/Autoload/Exception.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @category Library
+ * @package Tbs
+ * @subpackage Autoload
+ * @author Leonardo Thibes <eu@leonardothibes.com>
+ * @copyright Copyright (c) The Authors
+ */
+
+namespace Tbs\Autoload;
+
+/**
+ * @category Library
+ * @package Tbs
+ * @subpackage Autoload
+ * @author Leonardo Thibes <eu@leonardothibes.com>
+ * @copyright Copyright (c) The Authors
+ */
+class Exception extends \Exception
+{
+}
