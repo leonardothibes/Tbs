10d27612898a059dcce95e6a206e69e95d1109b7
Leonardo Thibes <eu@leonardothibes.com>
2013-07-30 11:29:03 -0300

    ajustes em identacao

--- tests/Tbs/VersionTest.php
+++ tests/Tbs/VersionTest.php
@@ -21,36 +21,36 @@ require_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'Bootstrap.php';
  */
 class VersionTest extends \PHPUnit_Framework_TestCase
 {
-	/**
-	 * @var \Tbs\Version
-	 */
-	protected $object = null;
+    /**
+     * @var \Tbs\Version
+     */
+    protected $object = null;
 
-	/**
-	 * Setup.
-	 */
-	public function setUp()
-	{
-		$this->object = new \Tbs\Version();
-	}
+    /**
+     * Setup.
+     */
+    public function setUp()
+    {
+    	$this->object = new \Tbs\Version();
+    }
 
-	/**
-	 * TearDown.
-	 */
-	public function tearDown()
-	{
-		unset($this->object);
-	}
+    /**
+     * TearDown.
+     */
+    public function tearDown()
+    {
+    	unset($this->object);
+    }
 
-	/**
-	 * @see \Tbs\Version::get()
-	 */
-	public function testGet()
-	{
-		$rs = v::get();
-		$this->assertInternalType('string', $rs);
+    /**
+     * @see \Tbs\Version::get()
+     */
+    public function testGet()
+    {
+        $rs = v::get();
+        $this->assertInternalType('string', $rs);
 
-		$version = file_get_contents(LIBRARY_PATH . '/Tbs/Version/Number.txt');
-		$this->assertEquals($version, $rs);
-	}
+        $version = file_get_contents(LIBRARY_PATH . '/Tbs/Version/Number.txt');
+        $this->assertEquals($version, $rs);
+    }
 }
