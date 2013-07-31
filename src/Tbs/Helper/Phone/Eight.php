00e0308694be639d54203607174c309d8e617b7d
Leonardo Thibes <eu@leonardothibes.com>
2013-07-31 16:55:04 -0300

    implementando helpers Phone para 8 e para 9 digitos #8

---
+++ src/Tbs/Helper/Phone/Eight.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * @category Library
+ * @package Tbs
+ * @subpackage Helper
+ * @author Leonardo Thibes <eu@leonardothibes.com>
+ * @copyright Copyright (c) The Authors
+ */
+
+namespace Tbs\Helper\Phone;
+
+use \Tbs\Helper\Interfaces\Mask     as M;
+use \Tbs\Helper\Interfaces\Validate as V;
+
+/**
+ * Helper functions for phones of 8 digits.
+ *
+ * @category Library
+ * @package Tbs
+ * @subpackage Helper
+ * @author Leonardo Thibes <eu@leonardothibes.com>
+ * @copyright Copyright (c) The Authors
+ */
+class Eight implements M, V
+{
+    /**
+     * Test if phone is valid.
+     *
+     * @param  string $phone
+     * @return bool
+     */
+    public static function isValid($phone)
+    {
+        return (bool)preg_match('/^\([0-9]{2}\)\s[0-9]{4}\-[0-9]{4}$/', $phone);
+    }
+
+    /**
+     * Sanitize the phone.
+     *
+     * @param  string $phone
+     * @return mixed
+    */
+    public static function sanitize($phone)
+    {
+        $sanitized = filter_var(strip_tags(self::unMask($phone)), FILTER_SANITIZE_NUMBER_INT);
+        return self::mask($sanitized);
+    }
+
+    /**
+     * Test if the phone is masked.
+     *
+     * @param  string $phone
+     * @return bool
+     */
+    public static function isMasked($phone)
+    {
+        //Nothing to do.
+        return self::isValid($phone);
+    }
+
+    /**
+     * Mask the phone.
+     *
+     * @param  string $phone
+     * @return string
+    */
+    public static function mask($phone)
+    {
+        return sprintf('(%d) %d-%d', substr($phone, 0, 2), substr($phone, 2, 4), substr($phone, 6, 4));
+    }
+
+    /**
+     * Unmask the phone.
+     *
+     * @param  string $phone
+     * @return string
+    */
+    public static function unMask($phone)
+    {
+        return str_replace(array('(', ')', '-', ' '), '', $phone);
+    }
+
+    /**
+     * Unmask the phone and convert the result in array.
+     *
+     * @param  string $phone
+     * @return string
+     */
+    public static function unMaskToArray($phone)
+    {
+        $phone = self::unMask($phone);
+        return array(
+            'ddd'   => substr($phone, 0, 2),
+            'phone' => substr($phone, 2, 8),
+        );
+    }
+}
---
+++ tests/Tbs/Helper/Phone/EightTest.php
@@ -0,0 +1,181 @@
+<?php
+/**
+ * @category Tests
+ * @package Tbs
+ * @subpackage Helper
+ * @author Leonardo Thibes <eu@leonardothibes.com>
+ * @copyright Copyright (c) The Authors
+ */
+
+namespace Tbs\Helper\Phone;
+use \Tbs\Helper\Phone\Eight as phone;
+require_once dirname(dirname(dirname(dirname(__FILE__)))) . DIRECTORY_SEPARATOR . 'Bootstrap.php';
+
+/**
+ * @category Tests
+ * @package Tbs
+ * @subpackage Helper
+ * @author Leonardo Thibes <eu@leonardothibes.com>
+ * @copyright Copyright (c) The Authors
+ */
+class EightTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Tbs\Helper\Phone\Eight
+     */
+    protected $object = null;
+
+    /**
+     * Setup.
+     */
+    public function setUp()
+    {
+    	$this->object = new phone();
+    }
+
+    /**
+     * TearDown.
+     */
+    public function tearDown()
+    {
+    	unset($this->object);
+    }
+
+    /**
+     * Test if implements the right interface.
+     */
+    public function testInterface()
+    {
+        $this->assertInstanceOf('\Tbs\Helper\Interfaces\Mask'    , $this->object);
+        $this->assertInstanceOf('\Tbs\Helper\Interfaces\Validate', $this->object);
+    }
+
+    /**
+     * Provider of valid phone numbers.
+     * @return array
+     */
+    public function providerValidPhones()
+    {
+        return array(
+            array('(11) 1234-5678'),
+            array('(11) 3188-9823'),
+            array('(51) 9987-7673'),
+        );
+    }
+
+    /**
+     * @see \Tbs\Helper\Phone\Eight::isValid()
+     * @dataProvider providerValidPhones
+     */
+    public function testIsValid($phone)
+    {
+        $rs = phone::isValid($phone);
+        $this->assertTrue($rs);
+    }
+
+    /**
+     * Provider of invalid phone numbers.
+     * @return array
+     */
+    public function providerInvalidPhones()
+    {
+        return array(
+            array(''),
+            array(null),
+            array(true),
+            array(false),
+            array('(11)3188-9823'),
+            array('(11) 31889823'),
+            array('(11) 3188 9823'),
+            array('(11)3188-9823'),
+            array('31889823'),
+        );
+    }
+
+    /**
+     * @see \Tbs\Helper\Phone\Eight::isValid()
+     * @dataProvider providerInvalidPhones
+     */
+    public function testIsInvalid($phone)
+    {
+        $rs = phone::isValid($phone);
+        $this->assertFalse($rs);
+    }
+
+    /**
+     * @see \Tbs\Helper\Phone\Eight::sanitize()
+     * @dataProvider providerValidPhones
+     */
+    public function testSanitizeEquals($phone)
+    {
+        $rs = phone::sanitize($phone);
+        $this->assertEquals($phone, $rs);
+    }
+
+    /**
+     * @see \Tbs\Helper\Phone\Eight::sanitize()
+     * @dataProvider providerValidPhones
+     */
+    public function testSanitizeTags($phone)
+    {
+        $rs = phone::sanitize($phone . '<script>alert("some content");</script>');
+        $this->assertEquals($phone, $rs);
+    }
+
+    /**
+     * @see \Tbs\Helper\Phone\Eight::isMasked()
+     * @dataProvider providerValidPhones
+     */
+    public function testIsMasked($phone)
+    {
+        $rs = phone::isMasked($phone);
+        $this->assertTrue($rs);
+    }
+
+    /**
+     * @see \Tbs\Helper\Phone\Eight::isMasked()
+     * @dataProvider providerInvalidPhones
+     */
+    public function testIsUnMasked($phone)
+    {
+        $rs = phone::isMasked($phone);
+        $this->assertFalse($rs);
+    }
+
+    /**
+     * @see \Tbs\Helper\Phone\Eight::mask()
+     */
+    public function testMask()
+    {
+        $rs = phone::mask('9912345678');
+        $this->assertInternalType('string', $rs);
+        $this->assertEquals('(99) 1234-5678', $rs);
+    }
+
+    /**
+     * @see \Tbs\Helper\Phone\Eight::unMask()
+     */
+    public function testUnMask()
+    {
+        $rs = phone::unMask('(99) 1234-5678');
+        $this->assertInternalType('string', $rs);
+        $this->assertEquals('9912345678', $rs);
+    }
+
+    /**
+     * @see \Tbs\Helper\Phone\Eight::unMaskToArray()
+     */
+    public function testUnMaskToArray()
+    {
+        $rs = phone::unMaskToArray('(99) 1234-5678');
+
+        $this->assertInternalType('array', $rs);
+        $this->assertEquals(2, count($rs));
+
+        $this->assertArrayHasKey('ddd', $rs);
+        $this->assertEquals('99', $rs['ddd']);
+
+        $this->assertArrayHasKey('phone', $rs);
+        $this->assertEquals('12345678', $rs['phone']);
+    }
+}
