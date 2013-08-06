d4fb774cd892dfe81bb97ca6dbf95943b6642863
Leonardo Thibes <eu@leonardothibes.com>
2013-08-06 11:55:04 -0300

    melhoria no tratamento de erro da classe \Tbs\Autoload

--- src/Tbs/Autoload.php
+++ src/Tbs/Autoload.php
@@ -50,7 +50,7 @@ class Autoload
         }
 
         $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-        if (!file_exists($fileName)) {
+        if (!self::fileExists($fileName)) {
             require_once 'Tbs/Autoload/Exception.php';
             throw new \Tbs\Autoload\Exception(
                 sprintf('Could not load file: "%s"', $fileName)
@@ -58,5 +58,23 @@ class Autoload
         }
 
         require_once $fileName;
+    }
+
+    /**
+     * Verify of file exists in include_path.
+     *
+     * @param  string $fileName
+     * @return bool
+     */
+    public static function fileExists($fileName)
+    {
+        $includePath = @explode(PATH_SEPARATOR, get_include_path());
+        foreach ($includePath as $include) {
+            $fileName = $include . '/' . $fileName;
+            if (file_exists($fileName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
