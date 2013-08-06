aa123d4d5803014102532258ba4f6ef8835fe40c
Leonardo Thibes <eu@leonardothibes.com>
2013-08-06 17:07:08 -0300

    helper number/money #7

---
+++ src/Tbs/Helper/Money.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @category Library
+ * @package Tbs
+ * @subpackage Helper
+ * @author Leonardo Thibes <eu@leonardothibes.com>
+ * @copyright Copyright (c) The Authors
+ */
+
+namespace Tbs\Helper;
+
+/**
+ * Money helper functions.
+ *
+ * @category Library
+ * @package Tbs
+ * @subpackage Helper
+ * @author Leonardo Thibes <eu@leonardothibes.com>
+ * @copyright Copyright (c) The Authors
+ */
+class Money
+{
+    /**
+     * Possible money formats.
+     */
+    const DOLAR = 'D';
+    const EURO  = 'E';
+    const REAL  = 'R';
+
+    /**
+     * Convert money to number format.
+     *
+     * @param float  $value
+     * @param string $format
+     *
+     * @return float|false
+     */
+    public static function toNumber($value, $format = self::REAL)
+    {
+        switch ($format) {
+            case self::REAL:
+            case self::EURO:
+                $value = str_replace('.', '' , $value);
+                $value = str_replace(',', '.', $value);
+                break;
+            case self::DOLAR:
+                $value = str_replace(',', '', $value);
+                break;
+            default:
+                return false;
+        }
+        return (float)number_format($value, 2, '.', '');
+    }
+}
---
+++ tests/Tbs/Helper/MoneyTest.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * @category Tests
+ * @package Tbs
+ * @subpackage Helper
+ * @author Leonardo Thibes <eu@leonardothibes.com>
+ * @copyright Copyright (c) The Authors
+ */
+
+namespace Tbs\Helper;
+use \Tbs\Helper\Money as M;
+require_once dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'Bootstrap.php';
+
+/**
+ * @category Tests
+ * @package Tbs
+ * @subpackage Helper
+ * @author Leonardo Thibes <eu@leonardothibes.com>
+ * @copyright Copyright (c) The Authors
+ */
+class MoneyTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Tbs\Helper\Money
+     */
+    protected $object = null;
+
+    /**
+     * Setup.
+     */
+    protected function setUp()
+    {
+    	$this->object = new M;
+    }
+
+    /**
+     * TearDown.
+     */
+    protected function tearDown()
+    {
+    	unset($this->object);
+    }
+
+    /**
+     * Provider of numbers to convert.
+     * @return array
+     */
+    public function providerMoneyToConvert()
+    {
+        return array(
+
+            //Dolar to Number.
+        	array('0.00'      , M::DOLAR, '0.00'),
+            array('1.11'      , M::DOLAR, '1.11'),
+            array('1.000'     , M::DOLAR, '1.00'),
+            array('1000'      , M::DOLAR, '1000.00'),
+            array('1234.56'   , M::DOLAR, '1234.56'),
+            array('1000000.00', M::DOLAR, '1000000.00'),
+            array('1000000'   , M::DOLAR, '1000000.00'),
+            array('0.000'     , M::DOLAR, '0.00'),
+            array('1.111'     , M::DOLAR, '1.11'),
+            //Dolar to Number.
+
+            //Real to Number.
+            array('0,00'        , M::REAL, '0.00'),
+            array('1,11'        , M::REAL, '1.11'),
+            array('1,00'        , M::REAL, '1.00'),
+            array('1000'        , M::REAL, '1000.00'),
+            array('1.234,56'    , M::REAL, '1234.56'),
+            array('1.000.000,00', M::REAL, '1000000.00'),
+            array('1.000.000'   , M::REAL, '1000000.00'),
+            array('0,000'       , M::REAL, '0.00'),
+            array('1,111'       , M::REAL, '1.11'),
+            //Real to Number.
+
+            //Euro to Number.
+            array('0,00'        , M::EURO, '0.00'),
+            array('1,11'        , M::EURO, '1.11'),
+            array('1,00'        , M::EURO, '1.00'),
+            array('1000'        , M::EURO, '1000.00'),
+            array('1.234,56'    , M::EURO, '1234.56'),
+            array('1.000.000,00', M::EURO, '1000000.00'),
+            array('1.000.000'   , M::EURO, '1000000.00'),
+            array('0,000'       , M::EURO, '0.00'),
+            array('1,111'       , M::EURO, '1.11'),
+            //Euro to Number.
+        );
+    }
+
+    /**
+     * @see \Tbs\Helper\Money::toNumber()
+     * @dataProvider providerMoneyToConvert
+     */
+    public function testToNumber($value, $format, $expected)
+    {
+        $rs = M::toNumber($value, $format);
+        $this->assertInternalType('float', $rs);
+        $this->assertEquals($expected, $rs);
+    }
+
+    /**
+     * Provider of numbers to default convert.
+     * @return array
+     */
+    public function providerMoneyToDefaultConvert()
+    {
+        return array(
+            array('0.00'      , '0,00'),
+            array('1.11'      , '1,11'),
+            array('1.000'     , '1,00'),
+            array('1000'      , '1.000,00'),
+            array('1234.56'   , '1.234,56'),
+            array('1000000.00', '1.000.000,00'),
+            array('1000000'   , '1.000.000,00'),
+            array('0.000'     , '0,00'),
+            array('1.111'     , '1,11'),
+        );
+    }
+
+    /**
+     * @see \Tbs\Helper\Money::toNumber()
+     * @dataProvider providerMoneysToDefaultConvert
+     */
+    public function estToMoneyDefaultConvert($number, $expected)
+    {
+        $rs = M::toMoney($number);
+        $this->assertInternalType('string', $rs);
+        $this->assertEquals($expected, $rs);
+    }
+
+/**
+     * @see \Tbs\Helper\Money::toNumber()
+     * @dataProvider providerMoneysToConvert
+     */
+    public function estToMoneyInvalidFormat($number, $format, $expected)
+    {
+        $rs = M::toMoney($number, 'invalid');
+        $this->assertFalse($rs);
+    }
+}
