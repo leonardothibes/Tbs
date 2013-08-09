e0791097116f8b32fa285bdf945e99504a0eb62c
Leonardo Thibes <eu@leonardothibes.com>
2013-08-09 10:11:52 -0300

    criando a classe \Tbs\DocBlock\Exception #58

---
+++ src/Tbs/DocBlock/Exception.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @category Library
+ * @package Tbs
+ * @subpackage Autoload
+ * @author Leonardo Thibes <eu@leonardothibes.com>
+ * @copyright Copyright (c) The Authors
+ */
+
+namespace Tbs\DocBlobk;
+
+use \Tbs\Exception as E;
+
+/**
+ * DocBlock Exception Class.
+ *
+ * @category Library
+ * @package Tbs
+ * @subpackage Autoload
+ * @author Leonardo Thibes <eu@leonardothibes.com>
+ * @copyright Copyright (c) The Authors
+ */
+class Exception extends E
+{
+}
