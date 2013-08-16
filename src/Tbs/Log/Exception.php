0b7baa82f457ab766d23ca2832f15d615e84aa34
Leonardo Thibes <eu@leonardothibes.com>
2013-08-16 11:29:06 -0300

    implementando exception #84

---
+++ src/Tbs/Log/Exception.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @category Library
+ * @package Tbs
+ * @subpackage Log
+ * @author Leonardo Thibes <eu@leonardothibes.com>
+ * @copyright Copyright (c) The Authors
+ */
+
+namespace Tbs\Log;
+
+use \Tbs\Exception as E;
+
+/**
+ * Log Exception Class.
+ *
+ * @category Library
+ * @package Tbs
+ * @subpackage DocLog
+ * @author Leonardo Thibes <eu@leonardothibes.com>
+ * @copyright Copyright (c) The Authors
+ */
+class Exception extends E
+{
+}
