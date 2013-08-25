0157a6398a49dd79e03ba1d7cdcae18da48f305c
Leonardo Thibes <leonardothibes@yahoo.com.br>
2013-08-25 16:41:11 -0300

    criando estrutura de diretorios, namespaces e classes em branco para iniciar a implementacao do componente "Tbs\Html" #13

---
+++ src/Tbs/Html/SpecialChars.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @category Library
+ * @package Tbs
+ * @subpackage Html
+ * @author Leonardo Thibes <eu@leonardothibes.com>
+ * @copyright Copyright (c) The Authors
+ */
+
+namespace Tbs\Html;
+
+/**
+ * Describes HTML special chars.
+ *
+ * @category Library
+ * @package Tbs
+ * @subpackage Html
+ * @author Leonardo Thibes <eu@leonardothibes.com>
+ * @copyright Copyright (c) The Authors
+ */
+class SpecialChars
+{
+	const TAB_DEFAUL    = "\11";
+	const TAB_PSR1      = '    ';
+	const TAB_PSR2      = '    ';
+	const LINE_END_UNIX = "\12";
+	const LINE_END_MAC  = "\15";
+	const LINE_END_WIN  = "\15\12";
+}
