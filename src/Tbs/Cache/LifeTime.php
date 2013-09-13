8c990cdaedaceda2c627926580c3fff16ba641fe
Leonardo Thibes <eu@leonardothibes.com>
2013-09-13 16:10:38 -0300

    implementando classe com constantes(em segundos) para os tempos de cache mais comuns #116

---
+++ src/Tbs/Cache/LifeTime.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package \Tbs\Cache
+ * @author Leonardo Thibes <eu@leonardothibes.com>
+ * @copyright Copyright (c) The Authors
+ */
+
+namespace Tbs\Cache;
+
+/**
+ * Cache Lifetime Constants.
+ *
+ * @package \Tbs\Cache
+ * @author Leonardo Thibes <eu@leonardothibes.com>
+ * @copyright Copyright (c) The Authors
+ */
+class LifeTime
+{
+    const HOUR_1  = 3600;
+    const HOUR_2  = 7200;
+    const HOUR_3  = 10800;
+    const HOUR_4  = 14400;
+    const HOUR_5  = 18000;
+    const HOUR_10 = 36000;
+    const HOUR_12 = 43200;
+    const HOUR_24 = 86400;
+    const HOUR_48 = 172800;
+    const HOUR_72 = 259200;
+    const WEEK    = 604800;
+}
