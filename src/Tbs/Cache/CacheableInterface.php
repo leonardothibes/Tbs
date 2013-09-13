0db576712eb24526ddb71bbfa0d1f6d13d7e8df4
Leonardo Thibes <eu@leonardothibes.com>
2013-09-13 16:07:41 -0300

    implementando interface para classes de cache #114

---
+++ src/Tbs/Cache/CacheableInterface.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @package \Tbs\Cache
+ * @author Leonardo Thibes <eu@leonardothibes.com>
+ * @copyright Copyright (c) The Authors
+ */
+
+namespace Tbs\Cache;
+
+/**
+ * Cacheable Interface.
+ *
+ * @package \Tbs\Cache
+ * @author Leonardo Thibes <eu@leonardothibes.com>
+ * @copyright Copyright (c) The Authors
+ */
+interface CacheableInterface
+{
+    /**
+     * Set the cache driver.
+     *
+     * @param string $driver   Name of driver(apc, memcached...).
+     * @param array  $frontend Frontend options of driver.
+     * @param array  $backend  Backend options of driver.
+     *
+     * @return CacheableInterface
+     */
+    public function setCacheDriver($driver = 'apc', array $frontend = array(), array $backend = array());
+
+    /**
+     * Get the cache driver.
+     * @return string
+     */
+    public function getCacheDriver();
+
+    /**
+     * Clear cache value.
+     *
+     * @param  string $id Cache id.
+     * @return Cacheable
+    */
+    public function clear($id = null);
+
+    /**
+     * Clear all the cache values.
+     * @return CacheableInterface
+     */
+    public function clearAll();
+}
