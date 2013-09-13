dcb7ab16bb1a7ffe161fb58462b8021ec3a94d9f
Leonardo Thibes <eu@leonardothibes.com>
2013-09-13 12:00:49 -0300

    implementando exception do componente de cache #115

---
+++ src/Tbs/Cache/Exception/CacheException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package \Tbs\Cache
+ * @author Leonardo Thibes <eu@leonardothibes.com>
+ * @copyright Copyright (c) The Authors
+ */
+
+namespace Tbs\Cache\Exception;
+
+use \Tbs\Exception as E;
+
+/**
+ * Exceptions of cache components.
+ *
+ * @package \Tbs\Cache
+ * @author Leonardo Thibes <eu@leonardothibes.com>
+ * @copyright Copyright (c) The Authors
+ */
+class CacheException extends E
+{
+}
