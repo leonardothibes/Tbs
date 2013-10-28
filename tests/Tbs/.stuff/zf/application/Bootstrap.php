6534058679ca1585c1f797d0176001a73346d1c9
Leonardo Thibes <eu@leonardothibes.com>
2013-10-28 17:06:09 -0200

    complementando a estrutura fake do ZF1

--- tests/Tbs/.stuff/zf/application/Bootstrap.php
+++ tests/Tbs/.stuff/zf/application/Bootstrap.php
@@ -1,1 +1,7 @@
 <?php
+
+use \Tbs\ZfComponents\V1\Bootstrap as B;
+
+class Bootstrap extends B
+{
+}
---
+++ tests/Tbs/.stuff/zf/application/default/Bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+class Default_Bootstrap
+{
+    public static function initStuff()
+    {
+        //Inithing nothing...
+    }
+}
