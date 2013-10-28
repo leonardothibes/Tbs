3d79094cb53c3b015cd28ae5b6f47642dae8fa1d
Leonardo Thibes <eu@leonardothibes.com>
2013-10-28 16:31:47 -0200

    adicionando estrutura de arquivos e diretorios fake do ZF1 aos testes

---
+++ tests/Tbs/.stuff/zf/application/default/controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'Zend/Controller/Action.php';
+
+class IndexController extends \Zend_Controller_Action
+{
+    public function init()
+    {
+        //Do nothing...
+    }
+
+    public function indexAction()
+    {
+        //Do nothing too.
+    }
+}
---
+++ tests/Tbs/.stuff/zf/application/default/forms/StuffForm.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'Zend/Form.php';
+
+class StuffForm extends \Zend_Form
+{
+    public function __construct()
+    {
+        //Do nothing...
+    }
+}
